3cffb75890ec5613072f03642e0d8080c2e124e2
ayseglozgr <70823020+ayseglozgr@users.noreply.github.com>
2020-11-10 01:45:25 +0300

    Overriding example
    programmr.com Polymorphism kısmından ornek

---
+++ Animal.java
@@ -0,0 +1,26 @@
+package animal;
+
+public class Animal {
+
+	public void move() {
+		System.out.println("Animals can move");
+	}
+}
+
+class Dog extends Animal {
+
+	public void move() {
+		System.out.println("Dogs can walk and run");
+	}
+}
+
+ class Challenge {
+
+	public static void main(String args[]) {
+               
+	Animal a = new Animal();
+        a.move();
+        Dog d = new Dog();
+        d.move();          
+	}
+}
